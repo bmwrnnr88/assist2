d300da7cd90e99f2dddf27e29b5f5247fbc6bd8a
bmwrnnr88 <138243329+bmwrnnr88@users.noreply.github.com>
2023-12-04 11:57:36 -0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,95 +11,81 @@ assistant_id = st.secrets["ASSISTANT_ID"]
 
 client = openai
 
-# Initialize session state variables for chat control
-if "start_chat" not in st.session_state:
-    st.session_state.start_chat = False
+def main():
 
-if "thread_id" not in st.session_state:
-    st.session_state.thread_id = None
+    if 'client' not in st.session_state:
+        # Initialize the client
+        st.session_state.client = openai.OpenAI()
 
-# Set up the Streamlit page with a title and icon
-st.set_page_config(page_title="ChatGPT-like Chat App", page_icon=":speech_balloon:")
+        st.session_state.file = st.session_state.client.files.create(
+            file=open("songs.txt", "rb"),
+            purpose='assistants'
+        )
 
-# Main chat interface setup
-st.title("OpenAI Assistants API Chat")
-st.write("This is a simple chat application that uses OpenAI's API to generate responses.")
+        # Step 1: Create an Assistant
+        st.session_state.assistant = st.session_state.client.beta.assistants.create(
+            name="Customer Service Assistant",
+            instructions="""You are the Sarcastic Vocab Wizard who assess the user on their knowledge of the assigned vocabulary words below. The Sarcastic Vocab Wizard is designed to combine a playful, mildly mocking tone with a trial-and-error approach to vocabulary learning. At the beginning of the quiz, the wizard will present a specific vocabulary word from the weekly list. The student is then asked to use this word in a sentence. The sentence must demonstrate knowledge of the word, meaning the sentence must be more than grammatically correct. The correct sentence must also have enough information that it demonstrates understanding of the word. If the sentence is not quite right, the wizard will provide sarcastic yet constructive feedback, encouraging the student to try again. The wizard allows multiple attempts before revealing an example, fostering independent learning. After going through all the words, the wizard will revisit any words that required revealing an example for another try. This approach ensures that humor is used to enhance the learning experience, while also making sure that students truly understand the words they are using., 
+            The assigned  vocabulary words are: Abate: (verb) to become less active, less intense, or less in amount. Example sentence: As I began my speech, my feelings of nervousness quickly abated​.,
+            Abstract: (adjective) existing purely in the mind; not representing actual reality. Example sentence: Julie had trouble understanding the appeal of the abstract painting​.,
+            Abysmal: (adjective) extremely bad. Example sentence: I got an abysmal grade on my research paper​ which ruined my summer vacation.,
+            Accordingly: (adverb) in accordance with. Example sentence: All students must behave accordingly, otherwise, they will receive harsh punishments​.,
+            Acquisition: (noun) the act of gaining a skill or possession of something. Example sentence: Language acquisition is easier for kids than it is for adults.,
+            Adapt: (verb) to make suit a new purpose; to accommodate oneself to a new condition, setting, or situation​. Begin all new conversations by greeting the user and then starting the vocab assessment.,
+            Adept: (adjective) having knowledge or skill (usually in a particular area). Example sentence: Beth loves playing the piano, but she’s especially adept at the violin​.,
+            Adequate: (adjective) having sufficient qualifications to meet a specific task or purpose. Example sentence: Though his resume was adequate, the company doubted whether he’d be a good fit​.,
+            Advent: (noun) the arrival or creation of something (usually historic). Example sentence: The world has never been the same since the advent of the light bulb​.,
+            Adversarial: (adjective) relating to hostile opposition. Example sentence: An adversarial attitude will make you many enemies in life​.""",
+            model="gpt-4-1106-preview",
+            file_ids=[st.session_state.file.id],
+            tools=[{"type": "retrieval"}]
+        )
 
-# Button to start the chat session
-if st.button("Start Chat"):
-    st.session_state.start_chat = True
-    # Initialize a thread for the chat session if it hasn't been done yet
-    if "thread_id" not in st.session_state or st.session_state.thread_id is None:
-        thread = client.beta.threads.create()
-        st.session_state.thread_id = thread.id
+        # Step 2: Create a Thread
+        st.session_state.thread = st.session_state.client.beta.threads.create()
 
-# Only show the chat interface if the chat has been started and a valid thread ID exists
-if st.session_state.start_chat and st.session_state.thread_id:
-    if "openai_model" not in st.session_state:
-        st.session_state.openai_model = "gpt-4-1106-preview"
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    user_query = st.text_input("Enter your query:", "I'm ready to begin the assessment")
 
-    # Display existing messages in the chat
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Chat input for the user
-    if prompt := st.chat_input("What is up?"):
-        # Add user message to the state and display it
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Add the user's message to the existing thread
-        client.beta.threads.messages.create(
-            thread_id=st.session_state.thread_id,
+    if st.button('Submit'):
+        # Step 3: Add a Message to a Thread
+        message = st.session_state.client.beta.threads.messages.create(
+            thread_id=st.session_state.thread.id,
             role="user",
-            content=prompt
+            content=user_query
         )
 
-        # Create a run with additional instructions
-        run = client.beta.threads.runs.create(
-            thread_id=st.session_state.thread_id,
-            assistant_id=assistant_id,
-            instructions="Please answer the queries using the knowledge provided in the files. When adding other information mark it clearly as such."
+        # Step 4: Run the Assistant
+        run = st.session_state.client.beta.threads.runs.create(
+            thread_id=st.session_state.thread.id,
+            assistant_id=st.session_state.assistant.id,
+            instructions="Please address the user as Mr. Ward's Favorite Student"
         )
 
-        # Poll for the run to complete and retrieve the assistant's messages
-        while run.status != 'completed':
-            time.sleep(1)
-            run = client.beta.threads.runs.retrieve(
-                thread_id=st.session_state.thread_id,
+        while True:
+            # Wait for 5 seconds
+            time.sleep(5)
+
+            # Retrieve the run status
+            run_status = st.session_state.client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread.id,
                 run_id=run.id
             )
 
-        # Retrieve messages added by the assistant
-        messages = client.beta.threads.messages.list(
-            thread_id=st.session_state.thread_id
-        )
+            # If run is completed, get messages
+            if run_status.status == 'completed':
+                messages = st.session_state.client.beta.threads.messages.list(
+                    thread_id=st.session_state.thread.id
+                )
 
-        # Process and display assistant messages
-        assistant_messages_for_run = [
-    message for message in messages 
-    if message.run_id == run.id and message.role == "assistant"
-]
-for message in assistant_messages_for_run:
-    # Initialize an empty string to accumulate message text
-    message_text = ""
+                # Loop through messages and print content based on role
+                for msg in messages.data:
+                    role = msg.role
+                    content = msg.content[0].text.value
+                    st.write(f"{role.capitalize()}: {content}")
+                break
+            else:
+                st.write("Waiting for the Assistant to process...")
+                time.sleep(5)
 
-    # Iterate over each item in the message.content list
-    for content_item in message.content:
-        if hasattr(content_item, 'text') and hasattr(content_item.text, 'value'):
-            message_text += content_item.text.value + " "  # Append the text value
-
-    # Trim any trailing whitespace from the accumulated message text
-    message_text = message_text.strip()
-
-    # Add the processed message to the state and display it
-    st.session_state.messages.append({"role": "assistant", "content": message_text})
-    with st.chat_message("assistant"):
-        st.markdown(message_text, unsafe_allow_html=True)
-#else:
-    # Prompt to start the chat
-    #st.write("Click 'Start Chat' to begin the conversation.")
+if __name__ == "__main__":
+    main()
